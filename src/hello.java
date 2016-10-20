14034986bacbbbfa28c4a408d276354f56e2a309
BrianAherne <brianaherne@live.com>
2016-10-20 10:47:13 +0100

    No interest amount calculation
    Added code to calculate the interest for a given:
    -principal
    -interestRate
    -number of years(numYears)

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,13 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+
+        int numYears = 10;
+        float principal = 10000.0f;
+        float interestRate = 5.0f;
+        //float bit = (float)Math.pow((1 + (interestRate/100)),(double)numYears);
+        float amount = (float) (principal * Math.pow((1 + (interestRate/100.0f)),numYears));
+
+        System.out.println(amount);
     }
 }
